1ef39d83d2bebf399aa932f1a180355b165c4184
Emmanuel Cousin <emmanuel@hey.com>
2025-10-08 17:55:30 -0300

    Update purchases_controller_spec.rb

--- spec/controllers/admin/purchases_controller_spec.rb
+++ spec/controllers/admin/purchases_controller_spec.rb
@@ -4,17 +4,19 @@ require "spec_helper"
 require "shared_examples/admin_base_controller_concern"
 
 describe Admin::PurchasesController, :vcr do
+  render_views
+
   it_behaves_like "inherits from Admin::BaseController"
 
+  let(:admin_user) { create(:admin_user) }
+
   before do
-    @admin_user = create(:admin_user)
-    sign_in @admin_user
+    create(:gumroad_merchant_account)
+    sign_in admin_user
   end
 
   describe "#show" do
-    before do
-      @purchase = create(:purchase)
-    end
+    let!(:purchase) { create(:purchase) }
 
     it "raises ActionController::RoutingError when purchase is not found" do
       expect { get :show, params: { id: "invalid-id" } }.to raise_error(ActionController::RoutingError)
@@ -23,80 +25,85 @@ describe Admin::PurchasesController, :vcr do
     it "finds the purchase correctly when loaded via external id" do
       expect(Purchase).not_to receive(:find_by_stripe_transaction_id)
       expect(Purchase).to receive(:find_by_external_id).and_call_original
-      get :show, params: { id: @purchase.external_id }
+      get :show, params: { id: purchase.external_id }
 
-      expect(assigns(:purchase)).to eq(@purchase)
-      assert_response :success
+      expect(response).to be_successful
+      expect(response.body).to include("data-page")
+      expect(response.body).to include("Admin/Purchases/Show")
+
+      expect(assigns(:purchase)).to eq(purchase)
     end
 
     it "finds the purchase correctly when loaded via id" do
       expect(Purchase).not_to receive(:find_by_stripe_transaction_id)
       expect(Purchase).not_to receive(:find_by_external_id)
-      get :show, params: { id: @purchase.id }
+      get :show, params: { id: purchase.id }
 
-      expect(assigns(:purchase)).to eq(@purchase)
-      assert_response :success
+      expect(assigns(:purchase)).to eq(purchase)
+      expect(response).to be_successful
     end
 
     it "finds the purchase correctly when loaded via numeric external id" do
       expect(Purchase).to receive(:find_by_external_id_numeric).and_call_original
-      get :show, params: { id: @purchase.external_id_numeric }
+      get :show, params: { id: purchase.external_id_numeric }
 
-      expect(assigns(:purchase)).to eq(@purchase)
-      assert_response :success
+      expect(assigns(:purchase)).to eq(purchase)
+      expect(response).to be_successful
     end
 
     it "finds the purchase correctly when loaded via stripe_transaction_id" do
       expect(Purchase).to receive(:find_by_stripe_transaction_id).and_call_original
-      get :show, params: { id: @purchase.stripe_transaction_id }
+      get :show, params: { id: purchase.stripe_transaction_id }
 
-      expect(assigns(:purchase)).to eq(@purchase)
-      assert_response :success
+      expect(assigns(:purchase)).to eq(purchase)
+      expect(response).to be_successful
     end
   end
 
   describe "POST refund_for_fraud" do
+    let(:purchase) { create(:purchase_in_progress, chargeable: create(:chargeable), purchaser: create(:user)) }
+
     before do
-      @purchase = create(:purchase_in_progress, chargeable: create(:chargeable), purchaser: create(:user))
-      @purchase.process!
-      @purchase.mark_successful!
+      purchase.process!
+      purchase.mark_successful!
     end
 
     it "refunds the purchase and blocks the buyer" do
-      comment_content = "Buyer blocked by Admin (#{@admin_user.email})"
+      comment_content = "Buyer blocked by Admin (#{admin_user.email})"
       expect do
-        post :refund_for_fraud, params: { id: @purchase.id }
+        post :refund_for_fraud, params: { id: purchase.id }
 
-        expect(@purchase.reload.stripe_refunded).to be(true)
-        expect(@purchase.buyer_blocked?).to eq(true)
+        expect(purchase.reload.stripe_refunded).to be(true)
+        expect(purchase.buyer_blocked?).to eq(true)
         expect(response).to be_successful
         expect(response.parsed_body["success"]).to be(true)
-      end.to change { @purchase.comments.where(content: comment_content, comment_type: "note", author_id: @admin_user.id).count }.by(1)
-       .and change { @purchase.purchaser.comments.where(content: comment_content, comment_type: "note", author_id: @admin_user.id, purchase: @purchase).count }.by(1)
+      end.to change { purchase.comments.where(content: comment_content, comment_type: "note", author_id: admin_user.id).count }.by(1)
+       .and change { purchase.purchaser.comments.where(content: comment_content, comment_type: "note", author_id: admin_user.id, purchase: purchase).count }.by(1)
     end
   end
 
   describe "POST refund_taxes_only" do
+    let(:purchase) { create(:purchase_in_progress, chargeable: create(:chargeable), purchaser: create(:user)) }
+
     before do
-      @purchase = create(:purchase_in_progress, chargeable: create(:chargeable), purchaser: create(:user))
-      @purchase.process!
-      @purchase.mark_successful!
+      purchase.process!
+      purchase.mark_successful!
     end
 
     it "successfully refunds taxes when refundable taxes are available" do
-      allow_any_instance_of(Purchase).to receive(:refund_gumroad_taxes!).with(refunding_user_id: @admin_user.id, note: nil, business_vat_id: nil).and_return(true)
+      allow_any_instance_of(Purchase).to receive(:refund_gumroad_taxes!).with(refunding_user_id: admin_user.id, note: nil, business_vat_id: nil).and_return(true)
 
-      post :refund_taxes_only, params: { id: @purchase.id }
+      post :refund_taxes_only, params: { id: purchase.id }
 
       expect(response).to be_successful
       expect(response.parsed_body["success"]).to be(true)
     end
 
     it "includes note and business_vat_id when provided" do
-      allow_any_instance_of(Purchase).to receive(:refund_gumroad_taxes!).with(refunding_user_id: @admin_user.id, note: "Tax exemption request", business_vat_id: "VAT123456").and_return(true)
+      allow_any_instance_of(Purchase).to receive(:refund_gumroad_taxes!).with(refunding_user_id: admin_user.id, note: "Tax exemption request", business_vat_id: "VAT123456").and_return(true)
 
       post :refund_taxes_only, params: {
-        id: @purchase.id,
+        id: purchase.id,
         note: "Tax exemption request",
         business_vat_id: "VAT123456"
       }
@@ -106,12 +113,12 @@ describe Admin::PurchasesController, :vcr do
     end
 
     it "returns error when tax refund fails" do
-      allow_any_instance_of(Purchase).to receive(:refund_gumroad_taxes!).with(refunding_user_id: @admin_user.id, note: nil, business_vat_id: nil).and_return(false)
+      allow_any_instance_of(Purchase).to receive(:refund_gumroad_taxes!).with(refunding_user_id: admin_user.id, note: nil, business_vat_id: nil).and_return(false)
       allow_any_instance_of(Purchase).to receive(:errors).and_return(
         double(full_messages: double(to_sentence: "Tax already refunded and Invalid tax amount"))
       )
 
-      post :refund_taxes_only, params: { id: @purchase.id }
+      post :refund_taxes_only, params: { id: purchase.id }
 
       expect(response).to be_successful
       expect(response.parsed_body["success"]).to be(false)
@@ -124,45 +131,48 @@ describe Admin::PurchasesController, :vcr do
   end
 
   describe "POST undelete" do
-    before do
-      @purchase = create(:purchase, purchaser: create(:user), is_deleted_by_buyer: true)
-    end
+    let(:purchaser) { create(:user) }
+    let(:is_deleted_by_buyer) { true }
+    let!(:purchase) { create(:purchase, purchaser:, is_deleted_by_buyer:) }
+    let(:comment_content) { "Purchase undeleted by Admin (#{admin_user.email})" }
 
     it "undeletes the purchase and creates comments" do
-      comment_content = "Purchase undeleted by Admin (#{@admin_user.email})"
       expect do
-        post :undelete, params: { id: @purchase.id }
+        post :undelete, params: { id: purchase.id }
 
-        expect(@purchase.reload.is_deleted_by_buyer).to be(false)
+        expect(purchase.reload.is_deleted_by_buyer).to be(false)
         expect(response).to be_successful
         expect(response.parsed_body["success"]).to be(true)
-      end.to change { @purchase.comments.where(content: comment_content, comment_type: "note", author_id: @admin_user.id).count }.by(1)
-       .and change { @purchase.purchaser.comments.where(content: comment_content, comment_type: "note", author_id: @admin_user.id, purchase: @purchase).count }.by(1)
+      end.to change { purchase.comments.where(content: comment_content, comment_type: "note", author_id: admin_user.id).count }.by(1)
+       .and change { purchase.purchaser.comments.where(content: comment_content, comment_type: "note", author_id: admin_user.id, purchase: purchase).count }.by(1)
     end
 
-    it "handles purchase that is already undeleted" do
-      @purchase.update!(is_deleted_by_buyer: false)
+    context "when purchase is already undeleted" do
+      let(:is_deleted_by_buyer) { false }
 
-      expect do
-        post :undelete, params: { id: @purchase.id }
+      it "does not change the purchase" do
+        expect do
+          post :undelete, params: { id: purchase.id }
 
-        expect(@purchase.reload.is_deleted_by_buyer).to be(false)
-        expect(response).to be_successful
-        expect(response.parsed_body["success"]).to be(true)
-      end.not_to change { @purchase.comments.count }
+          expect(purchase.reload.is_deleted_by_buyer).to be(false)
+          expect(response).to be_successful
+          expect(response.parsed_body["success"]).to be(true)
+        end.not_to change { purchase.comments.count }
+      end
     end
 
-    it "handles purchase without purchaser" do
-      @purchase.update!(purchaser: nil)
-      comment_content = "Purchase undeleted by Admin (#{@admin_user.email})"
+    context "when purchase without purchaser" do
+      let(:purchaser) { nil }
 
-      expect do
-        post :undelete, params: { id: @purchase.id }
+      it "undeletes the purchase" do
+        expect do
+          post :undelete, params: { id: purchase.id }
 
-        expect(@purchase.reload.is_deleted_by_buyer).to be(false)
-        expect(response).to be_successful
-        expect(response.parsed_body["success"]).to be(true)
-      end.to change { @purchase.comments.where(content: comment_content, comment_type: "note", author_id: @admin_user.id).count }.by(1)
+          expect(purchase.reload.is_deleted_by_buyer).to be(false)
+          expect(response).to be_successful
+          expect(response.parsed_body["success"]).to be(true)
+        end.to change { purchase.comments.where(content: comment_content, comment_type: "note", author_id: admin_user.id).count }.by(1)
+      end
     end
 
     it "raises error when purchase is not found" do
