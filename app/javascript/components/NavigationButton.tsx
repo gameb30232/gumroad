47001edea7b605f5637b8ec62020ab2a551e2cae
Pradumn <pradumn@tealfeed.com>
2025-10-14 02:00:11 +0530

    chore

--- app/javascript/components/NavigationButton.tsx
+++ app/javascript/components/NavigationButton.tsx
@@ -5,6 +5,10 @@ import { classNames } from "$app/utils/classNames";
 
 import { buttonVariants, NavigationButtonProps, useValidateClassName } from "$app/components/Button";
 
+/*
+    This component is for inertia specific navigation button,
+    since the other NavigationButton is used in a lot of ssr pages  and we can't import inertia Link there
+*/
 export const NavigationButtonInertia = React.forwardRef<HTMLAnchorElement, NavigationButtonProps>(
   ({ className, color, outline, small, disabled, children, ...props }, ref) => {
     useValidateClassName(className);
